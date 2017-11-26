379df282193fff564807d87aa7f71a0186ebdcb7
Grant Kayes <glk25@georgetown.edu>
2017-11-26 14:11:37 -0500

    add ideas route/page

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ const exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
 const app = express();
 
+//Bluebird promise - avoid warning
+mongoose.Promise = require('bluebird');
 //Connect to mongoose
 mongoose.connect('mongodb://localhost/vidjot-dev', {
 	useMongoClient: true
 })
 .then(() => console.log('MongoDB Connected...'))
 .catch(err => console.log(err));
+
+//Load idea model
+require('./models/Idea');
+const Idea = mongoose.model('ideas');
 
 const port = 5000;
 
@@ -29,7 +35,12 @@ app.get('/', (req, res) =>{
 //About route
 app.get('/about', (req, res) =>{
 	res.render('about');
-})
+});
+
+//Add idea form route
+app.get('/ideas/add', (req, res) =>{
+	res.render('ideas/add');
+});
 
 app.listen(port, () =>{
 	console.log(`Server started on ${port}`);
