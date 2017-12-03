8de883b356ae2c9745f8de1266afd7eff5205280
Grant Kayes <glk25@georgetown.edu>
2017-12-02 20:02:51 -0500

    fixing db

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 if (process.env.NODE_ENV === 'production') {
 	module.exports = {
-		mongoURI: 'mongodb://glk25:Weemiester!1@ds125906.mlab.com:25906/vidjot-prod'
+		mongoURI: 'mongodb://grant:grant@ds125906.mlab.com:25906/vidjot-prod'
 	}
 } else {
 	module.exports = {
